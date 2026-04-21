6a88867d4b717207285efa516a011ea944dc542f
Remi-Jakobi <remjandre@gmail.com>
2026-04-20 21:10:46 -0500

    Add function to create random stars on the screen

--- courtExtras.js
+++ courtExtras.js
@@ -130,3 +130,31 @@ export function addBleachers(scene) {
         scene.add(base);
     });
 }
+
+export function addStars() {
+  function random(min, max) {
+    return min + Math.random() * (max + 1 - min);
+  }
+
+  const body = document.querySelector('body');
+  const canvasSize = body.offsetWidth * body.offsetHeight;
+  const starsFraction = canvasSize / 2000;
+
+  for (let i = 0; i < starsFraction; i++) {
+    let xPos = random(0, 100);
+    let yPos = random(0, 100);
+    let alpha = random(0.5, 1);
+    let size = random(1, 2);
+    let colour = 'white';
+    const star = document.createElement('div');
+    star.style.position = 'fixed';
+    star.style.left = xPos + '%';
+    star.style.top = yPos + '%';
+    star.style.opacity = alpha;
+    star.style.width = size + 'px';
+    star.style.height = size + 'px';
+    star.style.backgroundColor = colour;
+    star.style.zIndex = '-1';
+    document.body.appendChild(star);
+  }
+}
